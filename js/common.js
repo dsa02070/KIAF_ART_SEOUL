0f1713a85ed850e7d0ea1a65ea4e27c5e868a8a3
gitname <82920762+dsa02070@users.noreply.github.com>
2021-06-18 23:32:55 +0900

    프라이머리 컬러 추가, border 수정
    1. section의 border의 색상 톤을 낮췄습니다.
    2. 단순 흑백의 디자인이었던 웹 사이트에 프라이머리 컬러를 추가했습니다.

--- js/common.js
+++ js/common.js
@@ -16,8 +16,11 @@ window.onscroll = function () {
 }
 
 // main banner img change window matchMedia event
-let mobileImg = window.matchMedia("screen and (max-width : 991px)");
-if (mobileImg.matches) {
-    document.querySelector('.carousel-item:nth-child(1) img').setAttribute('src', './img/mobile_main_banner01.jpg')
-    document.querySelector('.carousel-item:nth-child(2) img').setAttribute('src', './img/mobile_main_banner02.jpg')
+function mainBannerChange() {
+    let mobileImg = window.matchMedia("screen and (max-width : 991px)");
+    if (mobileImg.matches) {
+        document.querySelector('.carousel-item:nth-child(1) img').setAttribute('src', './img/mobile_main_banner01.jpg')
+        document.querySelector('.carousel-item:nth-child(2) img').setAttribute('src', './img/mobile_main_banner02.jpg')
+    }
 }
+mainBannerChange()
